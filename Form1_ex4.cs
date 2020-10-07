fa025631298ec2c2a1ae366825ee73c275eae7f0
Michael Bubka <63472004+Forgefill@users.noreply.github.com>
2020-10-07 23:12:45 +0300

    Add files via upload

---
+++ Form1_ex4.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Trg;
+namespace ДЗ_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private bool TCheck(string a, string b, string c)
+        {
+            double side1 = double.Parse(a);
+            double side2 = double.Parse(b);
+            double side3 = double.Parse(c);
+            if ((side1 > 0 && side2 > 0 && side3 > 0) && ((side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2)))
+            {
+                return true;
+            }
+            else return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TCheck(textBox1.Text, textBox2.Text, textBox3.Text)) 
+            {
+                label4.Text = "Трикутник добавлений";
+            }
+            else
+            {
+                label4.Text = "Трикутник не існує";
+                return;
+            }
+
+            int id = dataGridView1.Rows.Count-1;
+            dataGridView1.Rows.Add();
+           
+            if (textBox1.Text == textBox2.Text && textBox2.Text == textBox3.Text)
+            {
+                EquilateralTriangle tr = new EquilateralTriangle(double.Parse(textBox1.Text));
+                dataGridView1.Rows[id].Cells[0].Value = "Рівносторонній Трикутник";
+                dataGridView1.Rows[id].Cells[1].Value = textBox1.Text;
+                dataGridView1.Rows[id].Cells[2].Value = textBox2.Text;
+                dataGridView1.Rows[id].Cells[3].Value = textBox3.Text;
+                dataGridView1.Rows[id].Cells[4].Value = tr.GetAngleS12();
+                dataGridView1.Rows[id].Cells[5].Value = tr.GetAngleS13();
+                dataGridView1.Rows[id].Cells[6].Value = tr.GetAngleS23();
+                dataGridView1.Rows[id].Cells[7].Value = tr.GetPerimeter();
+                dataGridView1.Rows[id].Cells[8].Value = tr.GetArea();
+            }
+            else
+            {
+                Triangle tr = new Triangle(double.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text));
+                dataGridView1.Rows[id].Cells[0].Value = "Трикутник";
+                dataGridView1.Rows[id].Cells[1].Value = textBox1.Text;
+                dataGridView1.Rows[id].Cells[2].Value = textBox2.Text;
+                dataGridView1.Rows[id].Cells[3].Value = textBox3.Text;
+                dataGridView1.Rows[id].Cells[4].Value = tr.GetAngleS12();
+                dataGridView1.Rows[id].Cells[5].Value = tr.GetAngleS13();
+                dataGridView1.Rows[id].Cells[6].Value = tr.GetAngleS23();
+                dataGridView1.Rows[id].Cells[7].Value = tr.GetPerimeter();
+                dataGridView1.Rows[id].Cells[8].Value = "Unknown";
+            }
+            
+            }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar >= '0' && e.KeyChar<='9')
+            {
+                return;
+            }
+            if(e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+            if(e.KeyChar == ',')
+            {
+                if ((textBox1.Text.IndexOf(',') != -1) || (textBox1.Text.Length == 0))
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if(char.IsControl(e.KeyChar))
+            {
+
+                if ((e.KeyChar) == (char)Keys.Enter)
+                {
+                    if (sender.Equals(textBox1))
+                    {
+                        textBox2.Focus();
+                    }
+                    else
+                    {
+                        button1.Focus();
+                    }
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                return;
+            }
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+            if (e.KeyChar == ',')
+            {
+                if ((textBox2.Text.IndexOf(',') != -1) || (textBox2.Text.Length == 0))
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            if (char.IsControl(e.KeyChar))
+            {
+
+                if ((e.KeyChar) == (char)Keys.Enter)
+                {
+                    if (sender.Equals(textBox2))
+                    {
+                        textBox3.Focus();
+                    }
+                    else
+                    {
+                        button1.Focus();
+                    }
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                return;
+            }
+            if (e.KeyChar == '.')
+            {
+                e.KeyChar = ',';
+            }
+            if (e.KeyChar == ',')
+            {
+                if ((textBox3.Text.IndexOf(',') != -1) ||(textBox3.Text.Length == 0))
+                {
+                    e.Handled = true;
+                }
+                return;
+            }
+            
+            if (char.IsControl(e.KeyChar))
+            {
+
+                if ((e.KeyChar) == (char)Keys.Enter )
+                {
+                    if(sender.Equals(textBox3))
+                    {
+                        button1.Focus();
+                    }
+                    
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0 || textBox3.Text.Length == 0 )
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0 || textBox3.Text.Length == 0)
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0 || textBox2.Text.Length == 0 || textBox3.Text.Length == 0)
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Triangle_ex4.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trg
+{
+        class Triangle
+        {
+            private double side1;
+            private double side2;
+            private double side3;
+            public Triangle(double a, double b, double c)
+            {
+                side1 = a;
+                side2 = b;
+                side3 = c;
+            }
+            public double GetPerimeter()     //отримати периметр трикутника
+            {
+                return side1 + side2 + side3;
+            }
+            public void SetS1(double s1)    //зміна значеннь сторін
+            {
+                side1 = s1;
+            }
+            public void SetS2(double s2)
+            {
+                side2 = s2;
+            }
+            public void SetS3(double s3)
+            {
+                side3 = s3;
+            }
+            //методи для визначення кутів між відповідними сторонами
+            public double GetAngleS12()
+            {
+                return Angle(side1, side2, side3);
+            }
+            public double GetAngleS23()
+            {
+                return Angle(side2, side3, side1);
+            }
+            public double GetAngleS13()
+            {
+                return Angle(side1, side3, side2);
+            }
+            private double Angle(double a, double b, double c)
+            {
+
+                return Math.Acos((a * a + b * b - c * c) / (2 * a * b)) * 180 / Math.PI;
+            }
+
+        }
+
+        class EquilateralTriangle : Triangle
+        {
+            private double area;
+            public EquilateralTriangle(double a) : base(a, a, a)
+            {
+                area = a * a * Math.Sqrt(3) / 4;
+            }
+
+            public double GetArea()
+            {
+                return area;
+            }
+
+        }
+}
