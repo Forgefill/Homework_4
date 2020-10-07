2d751e341c2cfb73424fe644607fc53a48e317a4
Michael Bubka <63472004+Forgefill@users.noreply.github.com>
2020-10-07 23:16:21 +0300

    Add files via upload

---
+++ Figure_ex5.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figures
+{
+    abstract class Figure
+    {
+        public abstract double Perimeter();
+        public abstract double Area();
+        public abstract string GetFigureType();
+    }
+
+    class Triangle : Figure
+    {
+        private double side1;
+        private double side2;
+        private double side3;
+        public Triangle(double a, double b, double c)
+        {
+            side1 = a;
+            side2 = b;
+            side3 = c;
+        }
+        public override double Perimeter()
+        {
+            return side1 + side2 + side3;
+        }
+
+        public override double Area()
+        {
+            double p = Perimeter() / 2;
+            return Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+        }
+        public override string GetFigureType()
+        {
+            return "Трикутник";
+        }
+    }
+
+    class Circle : Figure
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public override double Area()
+        {
+            return Math.PI * radius * radius;
+        }
+        public override string GetFigureType()
+        {
+            return "Коло";
+        }
+    }
+    class Rectangle : Figure
+    {
+        double height;
+        double width;
+        public Rectangle(double a, double b)
+        {
+            height = a;
+            width = b;
+        }
+        public override double Area()
+        {
+            return height * width;
+        }
+        public override double Perimeter()
+        {
+            return 2 * (height + width);
+        }
+
+        public override string GetFigureType()
+        {
+            return "Прямокутник";
+        }
+    }
+
+    class Square : Figure
+    {
+        private double side;
+
+        public Square(double a)
+        {
+            side = a;
+        }
+        public override double Area()
+        {
+            return side * side;
+        }
+        public override double Perimeter()
+        {
+            return 2 * (side + side);
+        }
+
+        public override string GetFigureType()
+        {
+            return "Квадрат";
+        }
+    }
+
+    class Rhombus : Figure
+    {
+        private double side;
+        private double angle;
+        public Rhombus(double a, double angle)
+        {
+            side = a;
+            this.angle = angle;
+        }
+
+        public override double Area()
+        {
+            return side * side * Math.Sin(angle);
+        }
+
+        public override double Perimeter()
+        {
+            return 4 * side;
+        }
+
+        public override string GetFigureType()
+        {
+            return "Ромб";
+        }
+    }
+}
---
+++ Form1_ex5.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Figures;
+namespace Дз4_6
+{
+    public partial class Form1 : Form
+    {
+        List<Figure> figures = new List<Figure>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void AddToDGV(Figure f)
+        {
+            figures.Add(f);
+            int id = dataGridView1.Rows.Count - 1;
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[id].Cells[0].Value = f.GetFigureType();
+            dataGridView1.Rows[id].Cells[1].Value = f.Perimeter();
+            dataGridView1.Rows[id].Cells[2].Value = f.Area();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Figure f = new Triangle(double.Parse(textBox1.Text), double.Parse(textBox2.Text), double.Parse(textBox3.Text));
+            AddToDGV(f);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Figure f = new Circle(double.Parse(textBox4.Text));
+            AddToDGV(f);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Figure f = new Figures.Rectangle(double.Parse(textBox5.Text), double.Parse(textBox6.Text));
+            AddToDGV(f);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Figure f = new Square(double.Parse(textBox7.Text));
+            AddToDGV(f);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Figure f = new Rhombus(double.Parse(textBox8.Text), double.Parse(textBox9.Text));
+            AddToDGV(f);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
